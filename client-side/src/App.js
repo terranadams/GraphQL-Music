16cccfa7c2b1f66af9c643782d0d740dfe2f449f
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-03-18 17:08:19 -0600

    fetching documentation data on button click

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import GetQueryButton from "./components/GetQueryButton";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io",
+    cache: new InMemoryCache(),
+  });
+
+  const handleGet = () => {
+    client
+      .query({
+        query: gql`
+          query GetRates {
+            rates(currency: "USD") {
+              currency
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="buttonBox">
+      <GetQueryButton onClick={handleGet} />
     </div>
   );
 }
---
+++ client-side/src/components/GetQueryButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetQueryButton = (props) => {
+  return (
+    <button className="button" onClick={props.onClick}>Get some music!</button>
+  )
+}
+
+export default GetQueryButton
