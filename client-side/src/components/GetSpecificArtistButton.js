1e52fae1b15605b7b1347304081af42f348baeed
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-04-23 21:46:44 -0600

    getting input for querying with variable

--- client-side/src/components/GetSpecificArtistButton.js
+++ client-side/src/components/GetSpecificArtistButton.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { GET_SPECIFIC_ARTIST } from '../queries/queries'
 
 const GetSpecificArtistButton = () => {
 
+  const [input, setInput] = useState()
+
   const [getSpecificArtist, {loading, data}] = useLazyQuery(GET_SPECIFIC_ARTIST, {
-    variables: {id: 3}
+    variables: {id: parseInt(input)}
   })
 
   if (data) console.log('Get SPECIFIC artist: ',data)
@@ -13,10 +15,12 @@ const GetSpecificArtistButton = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     getSpecificArtist()
+    setInput('')
   }
 
   return (
     <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setInput(e.target.value)} value={input}></input>
         <button className='button'>Get Specific Artist</button>
     </form>
   )
